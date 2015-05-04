a46245512156f56ef73a9ad5e0af27ae291ead75
Jake Marwil <rjm02006@gmail.com>
2015-05-04 10:50:09 -0600

    toggle rotate class on record click and toggle the timer

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   $digi1 = document.getElementById("didgi4");
 
   $(".record").on("click", function() {
-    $('#stopwatch').timer();
+    if ($(this).hasClass("rotate")) {
+      $('#stopwatch').timer('pause');
+      $(this).toggleClass("rotate");
+    }
+    else {
+      $('#stopwatch').timer('resume');
+      $(this).toggleClass("rotate");
+    }
+
   })
 })
