5db0acfe2e8c4bd48aa7d274bce4aeec7bf72557
Jake Marwil <rjm02006@gmail.com>
2015-05-04 11:42:57 -0600

    play and pause audio tracks

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
   $digi1 = document.getElementById("didgi4");
 
+  var playingTracks = [];
+
   $(".record").on("click", function() {
+    var tracks = ['tracks/annoy.wav','tracks/didgi1.wav','tracks/didgi2.wav','tracks/didgi3.wav','tracks/didgi4.wav','tracks/entertain.wav'];
+    var track = tracks[Math.floor(Math.random() * tracks.length)];
+
     if ($(this).hasClass("rotate")) {
       $('#stopwatch').timer('pause');
       $(this).toggleClass("rotate");
+      stopTracks(playingTracks);
     }
     else {
       $('#stopwatch').timer('resume');
       $(this).toggleClass("rotate");
+      playingTracks.push(playTrack(track));
     }
+  });
 
-  })
-})
+  function playTrack(audio) {
+    var track = new Audio(audio);
+    track.play();
+    return track;
+  }
+
+  function stopTracks(audio) {
+    $.each (audio, function(i, el){
+      el.pause();
+    });
+  }
+
+});
